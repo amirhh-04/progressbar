d05946c1d8234f1efa2ebfb89a481822dbe43020
Amirhh_04 <04.amirhh@gmail.com>
2025-03-04 17:36:25 +0330

    Enhance progress bar styles and functionality; adjust dimensions, colors, and animations for better visibility and user experience.

--- html/js/script.js
+++ html/js/script.js
@@ -4,16 +4,17 @@ $('document').ready(function() {
     MythicProgBar.Progress = function(data) {
         $(".progress-container").css({"display":"block"});
         $("#progress-label").text(data.label);
-        $("#progress-bar").stop().css({"width": 0, "background-color": "#ff5f00"}).animate({
+        $("#progress-bar").stop().css({"width": 0, "background-color": "#ff3547", "box-shadow": "0 0 15px #ff3547", "border-radius": "100px"}).animate({
           width: '100%'
         }, {
           duration: parseInt(data.duration),
           complete: function() {
-            $(".progress-container").css({"display":"none"});
-            $("#progress-bar").css("width", 0);
-            $.post('http://mythic_progbar/actionFinish', JSON.stringify({
-                })
-            );
+            Com();
+            setTimeout(function () {
+                $(".progress-container").css({"display":"none"});
+                $("#progress-bar").css("width", 0);
+                $.post('http://mythic_progbar/actionFinish', JSON.stringify({ }));
+            }, 1000);
           }
         });
     };
@@ -21,7 +22,7 @@ $('document').ready(function() {
     MythicProgBar.ProgressCancel = function() {
         $(".progress-container").css({"display":"block"});
         $("#progress-label").text("CANCELLED");
-        $("#progress-bar").stop().css( {"width": "100%", "background-color": "#ff0000"});
+        $("#progress-bar").stop().css({"width": "100%", "background-color": "#ff8820", "box-shadow": "0 0 15px #ff8820", "border-radius": "100px"});
 
         setTimeout(function () {
             $(".progress-container").css({"display":"none"});
@@ -37,6 +38,19 @@ $('document').ready(function() {
         $(".character-box").removeClass('active-char');
         $(".character-box").attr("data-ischar", "false")
         $("#delete").css({"display":"none"});
+    };
+
+    Com = function() {
+        setTimeout(function () {
+            $(".progress-container").css({"display":"block"});
+            $("#progress-label").text("COMPLETE");
+            $("#progress-bar").stop().css({"width": "100%", "background-color": "#51ff00", "box-shadow": "0 0 15px #51ff008a", "border-radius": "100px"});
+        }, 500);
+
+        setTimeout(function () {
+            $(".progress-container").css({"display":"none"});
+            $("#progress-bar").css("width", 0);
+        }, 1000);
     };
     
     window.addEventListener('message', function(event) {
